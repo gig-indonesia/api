d36d18ac4b195f5a08b45ae77e0757db0619401e
myavidziqbal <sademark08@gmail.com>
2018-11-14 19:13:18 +0700

    Fix bug By commenting models

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const PORT = process.env.PORT;
 const express = require("express");
 const app = express();
 const accounts = require("./api/accounts");
-const hosts = require("./api/hosts");
-const artist = require("./api/artist");
-const gigApplied = require("./api/gigApplied");
-const gigCreated = require("./api/gigCreated");
-const notification = require("./api/notification");
+// const hosts = require("./api/hosts");
+// const artist = require("./api/artist");
+// const gigApplied = require("./api/gigApplied");
+// const gigCreated = require("./api/gigCreated");
+// const notification = require("./api/notification");
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // app.use("/products", products);
 // app.use("/customers", customers);
-// app.use("/accounts", accounts);
+app.use("/accounts", accounts);
 
 
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
